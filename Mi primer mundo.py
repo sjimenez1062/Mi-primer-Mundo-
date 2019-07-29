e9af235d669b9ca9503b393139f4e0b32e81e448
sjimenez1062 <53411563+sjimenez1062@users.noreply.github.com>
2019-07-29 16:43:09 -0500

    Create Mi primer mundo.py

---
+++ Mi primer mundo.py
@@ -0,0 +1,21 @@
+""" Descripción: Este progragma consiste en que reborg toma una manzana
+ubicado en la posicion (5,1), deja uana banana en la 
+misma posición y posteriormente regresa a su posicion 
+inicial home (1,1) """
+
+print ("Hola, yo soy reeeborg")
+print ("voy a hacer una misión")
+repeat 4:
+    move()
+#pause()
+take("apple")
+#pause()
+put("banana")
+#pause()
+repeat 2:
+    turn_left()
+#pause()
+repeat 4:
+    move()
+#Autor: Sergio Jiménez
+#Fecha: 19/07/2019
